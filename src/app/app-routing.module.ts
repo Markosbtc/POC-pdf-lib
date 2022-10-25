d548b054d6d5fa8c410368421e15fb92117941e3
Subotic Marko <suboticm@jusoft.hu>
2022-10-25 15:23:23 +0200

    pdf-lib

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PdfComponent } from './pdf/pdf.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: '',
+    component: PdfComponent,
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
